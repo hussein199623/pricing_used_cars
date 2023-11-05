fc4cee2d585585ce8d16e1fd5b83bd87d4528c68
Hussein Alzeer <102005625+hussein199623@users.noreply.github.com>
2023-11-05 04:33:49 +0200

    Update home.py

--- home.py
+++ home.py
@@ -12,20 +12,20 @@ def app():
    st.subheader("***'We have a data about the price for used cars and we got it from site for selling used cars'***")
        
 
-   st.header('***Put the range of the price and you will get [Manufacturer name,drivetrian,model]***')
-   range=st.button('Put The Range')
-   df=pd.read_csv('new_data_cars.csv')
+   # st.header('***Put the range of the price and you will get [Manufacturer name,drivetrian,model]***')
+   # range=st.button('Put The Range')
+   # df=pd.read_csv('new_data_cars.csv')
    
 
-   # Function to filter the DataFrame based on price range
-   def filter_cars_by_price(df, min_price, max_price):
-      filtered_df = df[['manufacturer', 'model', 'drivetrain', 'price']]
-      filtered_df = filtered_df[(filtered_df['price'].between(min_price, max_price))]
-      return filtered_df
+   # # Function to filter the DataFrame based on price range
+   # def filter_cars_by_price(df, min_price, max_price):
+   #    filtered_df = df[['manufacturer', 'model', 'drivetrain', 'price']]
+   #    filtered_df = filtered_df[(filtered_df['price'].between(min_price, max_price))]
+   #    return filtered_df
    
-   # Define the price range
-   min_price = 260
-   max_price = 65500
+   # # Define the price range
+   # min_price = 260
+   # max_price = 65500
 
    # Create a slider for selecting the price range
    selected_price_range = st.slider('Select Price Range', min_value=min_price, max_value=max_price, value=(min_price, max_price))
