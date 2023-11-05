ee2dab0e653aa17dd99526a189fbca56844aa1db
Hussein Alzeer <102005625+hussein199623@users.noreply.github.com>
2023-11-05 05:11:23 +0200

    Update home.py

--- home.py
+++ home.py
@@ -1,44 +1,39 @@
-import streamlit as st 
-import pandas as pd 
-import plotly.express as px
+import streamlit as st
+import pandas as pd
+
+@st.cache_data
+def load_data():
+    columns = ['manufacturer', 'model', 'drivetrain', 'price']  # Add all the columns you need
+    df = pd.read_csv('new_data_cars.csv', usecols=columns)
+    return df
+
+def filter_cars_by_price(df, min_price, max_price):
+    return df[(df['price'] >= min_price) & (df['price'] <= max_price)]
 
 def app():
-   st.title("Pricing Used Cars")
+    st.title("Pricing Used Cars")
+    
+    # Load the data
+    df = load_data()
 
-# Apply CSS styling to center the title
-  
-
-   st.image('photo.jpg', width=500,use_column_width=True)
-   st.subheader("***'We have a data about the price for used cars and we got it from site for selling used cars'***")
+    st.image('photo.jpg', width=500, use_column_width=True)
+    st.subheader("***'We have data about the price for used cars obtained from a site for selling used cars'***")
        
-
-   st.header('***Put the range of the price and you will get [Manufacturer name,drivetrian,model]***')
-   range=st.button('Put The Range')
-   df=pd.read_csv('new_data_cars.csv')
+    st.header('***Put the range of the price and you will get [Manufacturer name, drivetrain, model]***')
    
+    # Define the price range
+    min_price = 260
+    max_price = 65500
 
-   # Function to filter the DataFrame based on price range
-   def filter_cars_by_price(df, min_price, max_price):
-      filtered_df = df[['manufacturer', 'model', 'drivetrain', 'price']]
-      filtered_df = filtered_df[(filtered_df['price'].between(min_price, max_price))]
-      return filtered_df
-   
-   # Define the price range
-   min_price = 260
-   max_price = 65500
+    # Create a slider for selecting the price range
+    selected_price_range = st.slider('Select Price Range', min_value=min_price, max_value=max_price, value=(min_price, max_price))
 
-   Create a slider for selecting the price range
-   selected_price_range = st.slider('Select Price Range', min_value=min_price, max_value=max_price, value=(min_price, max_price))
+    # Filter the car dataset based on the selected price range
+    filtered_cars = filter_cars_by_price(df, selected_price_range[0], selected_price_range[1])
 
-   # Filter the car dataset based on the selected price range
-   filtered_cars = filter_cars_by_price(df, selected_price_range[0], selected_price_range[1])
+    # Display the filtered dataset
+    st.write(filtered_cars)
 
-   # Display the filtered dataset
-   st.write(filtered_cars[['manufacturer', 'model', 'drivetrain', 'price']])
-
-
-   
-
-
-   
-
+# Run the app
+if __name__ == '__main__':
+    app()
