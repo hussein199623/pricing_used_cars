f7f5c6010a8471439be6af848dc6c2ca67a6f31e
Hussein Alzeer <102005625+hussein199623@users.noreply.github.com>
2023-11-05 06:47:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,64 +1,9 @@
-# import streamlit as st
-
-# from streamlit_option_menu import option_menu
-
-
-# import home, analysis, prediction, about, account
-# st.set_page_config(
-#         page_title="Main Menu",
-# )
-# pages = ['Home', 'Account', 'About', 'Analysis', 'Prediction']
-
-
-
-# class MultiApp:
-
-#     def __init__(self):
-#         self.apps = []
-
-#     def add_app(self, title, func):
-
-#         self.apps.append({
-#             "title": title,
-#             "function": func
-#         })
-
-#     def run():
-#         # app = st.sidebar(
-#         with st.sidebar:        
-#             app = option_menu(
-#                 menu_title='Main Menu ',
-#                 options=['Home','Account','About','Analysis','Prediction'],
-#                 icons=['house-fill','person-circle','info-circle-fill','chat-fill','trophy-fill'],
-#                 menu_icon='chat-text-fill',
-#                 default_index=1,
-#                 styles={
-#                     "container": {"padding": "5!important","background-color":'black'},
-#         "icon": {"color": "white", "font-size": "23px"}, 
-#         "nav-link": {"color":"white","font-size": "20px", "text-align": "left", "margin":"0px", "--hover-color": "red"},
-#         "nav-link-selected": {"background-color": "#02ab21"},}
-                
-#                 )
-
-        
-#         if app == "Home":
-#             home.app() 
-#         if app == "Account":
-#             account.app() 
-#         if app == 'About':
-#             about.app()   
-#         if app == "Analysis":
-#             analysis.app()        
-#         if app == 'Prediction':
-#             prediction.app()
-           
-
-
-
 
 import streamlit as st
 from streamlit_option_menu import option_menu
 
+
+@st.cache_data
 st.set_page_config(page_title="Main Menu")
 
 pages = ['Home', 'Account', 'About', 'Analysis', 'Prediction']
